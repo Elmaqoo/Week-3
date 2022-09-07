91d1b06b33a14c0430928ea0181f9d710c80a9a5
Elmaqoo <110962513+Elmaqoo@users.noreply.github.com>
2022-09-07 14:44:29 +1000

    Update main.js

--- main.js
+++ main.js
@@ -4,20 +4,21 @@ const text3 = '     JavaScript    ';
 
 // concatenating two strings
 const result1 = text1.concat(' ', text2);
-console.log(result1);
+console.log(result1); // "hello world"
 
 // converting the text to uppercase
 const result2 = text1.toUpperCase();
-console.log(result2);
+console.log(result2); // HELLO
 
 // removing whitespace from the string
 const result3 = text3.trim();
-console.log(result3);
+console.log(result3); // JavaScript
 
-// converting the string to an array
-const result4 = text1.split();
-console.log(result4);
+// returns whether searching String may be found in the source string
+const result4 = text1.includes("o");
+console.log(result4); // ["hello"]
 
 // slicing the string
 const result5= text1.slice(1, 3);
-console.log(result5);
+console.log(result5); // "el"
+
