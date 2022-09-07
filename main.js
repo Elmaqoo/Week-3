74774a3a21b71057836053537aa59cc79e9a0ec8
Elmaqoo <110962513+Elmaqoo@users.noreply.github.com>
2022-09-07 13:50:34 +1000

    Update main.js

--- main.js
+++ main.js
@@ -4,20 +4,20 @@ const text3 = '     JavaScript    ';
 
 // concatenating two strings
 const result1 = text1.concat(' ', text2);
-console.log(result1); // "hello world"
+console.log(result1);
 
 // converting the text to uppercase
 const result2 = text1.toUpperCase();
-console.log(result2); // HELLO
+console.log(result2);
 
 // removing whitespace from the string
 const result3 = text3.trim();
-console.log(result3); // JavaScript
+console.log(result3);
 
 // converting the string to an array
 const result4 = text1.split();
-console.log(result4); // ["hello"]
+console.log(result4);
 
 // slicing the string
 const result5= text1.slice(1, 3);
-console.log(result5); // "el"
+console.log(result5);
